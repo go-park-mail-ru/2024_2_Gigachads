6426c9386b3c596ce655a8cc938055d66309dbb7
Софья <[sonya_pr@bk.ru]>
2024-10-27 20:59:18 +0300

    migrations and fix drawio

---
+++ db/migrations/migrations.sql
@@ -0,0 +1,47 @@
+-- Создание таблицы пользователей (user)
+CREATE TABLE IF NOT EXISTS profile (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    email TEXT NOT NULL UNIQUE CHECK (LENGTH(email) <= 50),
+    username TEXT NOT NULL CHECK (LENGTH(username) >= 6 AND LENGTH(username) <= 50),
+    password TEXT NOT NULL CHECK (LENGTH(password) >= 6 AND LENGTH(password) <= 50),
+    avatar_url TEXT DEFAULT NULL
+);
+
+-- Создание таблицы писем (message)
+CREATE TABLE IF NOT EXISTS message (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    title TEXT NOT NULL CHECK (LENGTH(title) <= 100),
+    description TEXT DEFAULT NULL,
+);
+
+-- Создание таблицы писем пользователей (email_transaction)
+CREATE TABLE IF NOT EXISTS email_transaction (
+	id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	parent_transaction_id INTEGER DEFAULT NULL,
+	sender_email TEXT,
+    recipient_email TEXT,
+    message_id INTEGER,
+    sending_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    isRead BOOLEAN NOT NULL DEFAULT FALSE,
+    folder_id INTEGER,
+    CONSTRAINT fk_sender FOREIGN KEY (sender_email) REFERENCES profile(email) ON DELETE SET NULL,
+    CONSTRAINT fk_recipient FOREIGN KEY (recipient_email) REFERENCES profile(email) ON DELETE NULL,
+    CONSTRAINT fk_message FOREIGN KEY (message_id)  REFERENCES message(id),
+    CONSTRAINT fk_folder FOREIGN KEY (folder_id)  REFERENCES folder(id) ON DELETE CASCADE
+);
+
+-- Создание таблицы папок (folder)
+CREATE TABLE IF NOT EXISTS folder (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id INTEGER,
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES profile(id) ON DELETE CASCADE,
+    name TEXT NOT NULL CHECK (LENGTH(name) <= 50)
+);
+
+-- Создание таблицы вложений (attachment)
+CREATE TABLE IF NOT EXISTS attachment (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    message_id INTEGER,
+    url TEXT NOT NULL,
+    CONSTRAINT fk_message FOREIGN KEY  (message_id) REFERENCES message(id) ON DELETE CASCADE
+);
