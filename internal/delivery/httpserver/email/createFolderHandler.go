513725d2168e2c405c9b1b7c811c50bd6d20e053
sychnico <107313617+sychnico@users.noreply.github.com>
2024-11-24 16:14:18 +0300

    create and delete folder now use path parameters

--- internal/delivery/httpserver/email/createFolderHandler.go
+++ internal/delivery/httpserver/email/createFolderHandler.go
@@ -1,12 +1,12 @@
 package email
 
 import (
-	"encoding/json"
+	//"encoding/json"
 	"mail/pkg/utils"
 	"net/http"
 	"mail/internal/models"
+	"github.com/gorilla/mux"
 )
-
 
 func (er *EmailRouter) CreateFolderHandler(w http.ResponseWriter, r *http.Request) {
 	ctxEmail := r.Context().Value("email")
@@ -16,22 +16,21 @@ func (er *EmailRouter) CreateFolderHandler(w http.ResponseWriter, r *http.Reques
 	}
 	email := ctxEmail.(string)
 
-	var folder models.Folder
-	err := json.NewDecoder(r.Body).Decode(&folder)
-	
-	if err != nil {
-		utils.ErrorResponse(w, r, http.StatusBadRequest, "invalid_json")
+	vars := mux.Vars(r)
+	folderName, ok := vars["name"]
+	if !ok {
+		utils.ErrorResponse(w, r, http.StatusBadRequest, "invalid_path")
 		return
 	}
 
-	folder.Name = utils.Sanitize(folder.Name)
+	folderName = utils.Sanitize(folderName)
 		
-	if !models.InputIsValid(folder.Name) {
+	if !models.InputIsValid(folderName) {
 		utils.ErrorResponse(w, r, http.StatusBadRequest, "invalid_name")
 		return
 	}
 
-	err = er.EmailUseCase.CreateFolder(email, folder.Name)
+	err := er.EmailUseCase.CreateFolder(email, folderName)
 	if err != nil {
 		utils.ErrorResponse(w, r, http.StatusInternalServerError, err.Error())
 		return
