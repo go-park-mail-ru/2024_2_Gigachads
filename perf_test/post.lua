e1aa0c4579d6f1fbf429587b6f0e52bc5e94c2d0
Софья <[sonya_pr@bk.ru]>
2024-12-22 15:10:56 +0300

    post lua

---
+++ perf_test/post.lua
@@ -0,0 +1,34 @@
+local wrk = require "wrk"
+local json = require("cjson")
+
+------------генерация рандомной строки
+math.randomseed(os.time())  -- Инициализация генератора случайных чисел
+
+local function randomString(length)
+    local chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    local result = ''
+    for i = 1, length do
+        local index = math.random(1, #chars)
+        result = result .. chars:sub(index, index)
+    end
+    return result
+end
+-------------------------------------
+
+wrk.method = "POST"
+wrk.headers["Content-Type"] = "application/json"
+wrk.headers["Cookie"] = "email=b7698ba355803ae8ac44993d9283e47;csrf=b12ca9de73d413ec524cfa80fbd9432c"
+
+wrk.requests = 100000
+
+wrk.thread = function()
+    local titleRand = randomString(10)
+    local descriptionRand = randomString(200)
+    wrk.body = json.encode({
+       parentID = 0,
+       recipient = "sonya@giga-mail.ru",
+       title = titleRand,
+       description = descriptionRand
+    })
+    wrk.connections = 10 -- Количество одновременных подключений на поток
+end
